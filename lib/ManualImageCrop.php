c8b2c306b1db0557a19f0ec70e9490ebd4c1ba4b
Chauncey McAskill <chauncey@locomotive.ca>
2018-09-18 17:20:21 -0400

    Move hook "mic_crop_done"
    Changed:
    - Move action hook "mic_crop_done" from `ManualImageCrop::cropImage()` to `ManualImageCrop::cropSuccess()`
    
    Amends: 14a5a0ce813475a3078ff3e174de492ea0cc32b7

--- lib/ManualImageCrop.php
+++ lib/ManualImageCrop.php
@@ -189,6 +189,10 @@ class ManualImageCrop {
 			wp_update_attachment_metadata($data['attachmentId'], $imageMetadata);
 		}
 
+		// run an action that other scripts can hook into, letting them
+		// know that the cropping is done for the given image
+		do_action('mic_crop_done', $data, $imageMetadata);
+
 		//returns the url to the generated image (to allow refreshing the preview)
 		if ( $silent_result ) {
 			return;
@@ -493,10 +497,6 @@ class ManualImageCrop {
 				}
 			}
 		}
-
-		// run an action that other scripts can hook into, letting them
-		// know that the cropping is done for the given image
-		do_action('mic_crop_done', $data, $imageMetadata);
 
 		$this->cropSuccess( $data, $dst_file_url, $silent_result );
 	}
